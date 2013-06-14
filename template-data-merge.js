ab13ea264d0cbf04f54dc0abcd7fa278c03875ee
Mark Hebert <mark@hasissues.com>
2013-06-14 11:13:09 -0400

    Better ASYNC

--- template-data-merge.js
+++ template-data-merge.js
@@ -3,56 +3,62 @@ var azure = require('azure');
 var fs = require("fs");
 var xml = require("node-xml");
 
-var path = { params: [], fileName: "", templateName: "", templateConfigName: "", contentFileName: "", path: "", querystring: "", pagePath: "" };
-var response = { statusCode: 500, contentType: "text/html", template: "LOADING", templateConfig: "LOADING", content: "LOADING", masterPage: "LOADING", data: "", html: "" };
-var req = null;
-var res = null;
-var templateDir = null;
-
 exports.templateDataMerge = function (processRequest, processResponse, domain, settings, useCloudData, configName) {
+	var path = { params: [], fileName: "", templateName: "", templateConfigName: "", contentFileName: "", path: "", querystring: "", pagePath: "" };
+	var response = { statusCode: 500, contentType: "text/html", template: "LOADING", templateConfig: "LOADING", content: "LOADING", masterPage: "LOADING", data: "", html: "" };
 	response.html = "<h1>No Content Found for " + domain + ".</h1>";
 	templateDir = "templates/" + domain.replace(".", "-");
-	req = processRequest;
-	res = processResponse;
-	parseURL(req.url);
+	parseURL(processRequest, path, response);
+	console.log("REQUEST: " + path.fileName);
 	if (path.fileName == "publish.htm" && configName == "LOCAL") {
 		var pub = require("./template-push-to-blob.js");
-		pub.publishTemplates(processRequest, processResponse, domain, settings, useCloudData, configName);
+		pub.publishTemplates(processRequest, processResponse, path, response, domain, settings, useCloudData, configName);
 	} else {
 		if (path.fileName.indexOf(".htm") > 0) {
-			load(domain, settings, useCloudData, configName);
+			load(processRequest, processResponse, path, response, domain, settings, useCloudData, configName);
 		} else {
-			if (path.fileName.indexOf(".css") > 0) {
-				try {
-					res.writeHead(200, { 'Content-Type': 'text/css' });
-					res.end(fs.readFileSync(templateDir + "/css/" + path.fileName, "utf8"));
-				} catch (e) {
-					res.writeHead(404, { 'Content-Type': 'text/css', 'error': 'File Not Found.' });
-					res.end("OPPS");
+			//-- static files
+			var filePath = null;
+			var fileEncode = "utf8";
+			if (path.path == "/client-scripts") {
+				filePath = "./client-scripts/" + path.fileName;
+				if (path.fileName.indexOf(".js") > 0) {
+					response.contentType = "text/javascript";
+				} else if (path.fileName.indexOf(".css") > 0) {
+					response.contentType = "text/css";
 				}
-			} else if (path.path == "/client-scripts") {
-				try {
-					res.writeHead(200, { 'Content-Type': 'text/javascript' });
-					res.end(fs.readFileSync("./client-scripts/" + path.fileName, "utf8"));
-				} catch (e) {
-					res.writeHead(404, { 'Content-Type': 'text/javascript', 'error': 'File Not Found.' });
-					res.end("OPPS");
-				}
+			} else if (path.fileName.indexOf(".css") > 0) {
+				filePath = templateDir + "/css/" + path.fileName;
+				response.contentType = "text/css";
 			} else if (path.fileName.indexOf(".js") > 0) {
-				try {
-					res.writeHead(200, { 'Content-Type': 'text/javascript' });
-					res.end(fs.readFileSync(templateDir + "/scripts/" + path.fileName, "utf8"));
-				} catch (e) {
-					res.writeHead(404, { 'Content-Type': 'text/javascript', 'error': 'File Not Found.' });
-					res.end("OPPS");
-				}
+				filePath = templateDir + "/scripts/" + path.fileName;
+				response.contentType = "text/javascript";
+			} else if (path.fileName.indexOf(".gif") > 0) {
+				filePath = templateDir + "/images/" + path.fileName;
+				response.contentType = "image/gif";
+				fileEncode = null;
+			}
+			//-- go get the file
+			if (filePath != null) {
+				fs.readFile(filePath, fileEncode, function (err, data) {
+					console.log("RESPONSE: " + path.fileName);
+					if (err) {
+						processResponse.writeHead(404, { 'Content-Type': response.contentType, 'error': 'File Not Found.' });
+						processResponse.end("OPPS");
+					} else {
+						processResponse.writeHead(200, { 'Content-Type': response.contentType });
+						processResponse.end(data);
+					}
+				});
 			} else {
-				res.end("NONE");
+				processResponse.writeHead(404, { 'Content-Type': response.contentType, 'error': 'File Not Found.' });
+				processResponse.end("NONE");
 			}
 		}
 	}
 };
-var load = function (domain, settings, useCloudData, configName) {
+
+var load = function (processRequest, processResponse, path, response, domain, settings, useCloudData, configName) {
 	response.statusCode = 200;
 	if (useCloudData) {
 		var blobService = null;
@@ -68,13 +74,13 @@ var load = function (domain, settings, useCloudData, configName) {
 		blobService.getBlobToText(domain.replace(".", "-"), path.templateName, 
 			function (error, text, blockBlob) {
 				if (error == null) { response.template = text;} else { response.template = "NONE"; console.log("\n" + error); }
-				merge();
+				merge(processRequest, processResponse, path, response);
 				//-- do we have a master page
 				var tQ = cheerio.load(response.template);
 				blobService.getBlobToText(domain.replace(".", "-"), tQ("head").attr("masterPage") + ".htm", 
 					function (error, text, blockBlob) {
 						if (error == null) { response.masterPage = text;} else { response.masterPage = "NONE"; console.log("\n" + error); }
-						merge();
+						merge(processRequest, processResponse, path, response);
 					}
 				);
 			}
@@ -83,35 +89,57 @@ var load = function (domain, settings, useCloudData, configName) {
 		blobService.getBlobToText(domain.replace(".", "-"), path.templateConfigName, 
 			function (error, text, blockBlob) {
 				if (error == null) { response.templateConfig = text;} else { response.templateConfig = "NONE"; console.log("\n" + error); }
-				merge();
+				merge(processRequest, processResponse, path, response);
 			}
 		);
 		//-- load the content
 		blobService.getBlobToText(domain.replace(".", "-"), path.contentFileName, 
 			function (error, text, blockBlob) {
 				if (error == null) { response.content = text;} else { response.content = "NONE"; console.log("\n" + error); }
-				merge();
+				merge(processRequest, processResponse, path, response);
 			}
 		);
 	} else {
-		//-- load the templates
-		try {
-			response.template = fs.readFileSync(templateDir + "/" + path.templateName, "utf8"); 
-			//-- do we have a master page
-			var tQ = cheerio.load(response.template);
-			try { response.masterPage = fs.readFileSync(templateDir + "/" + tQ("head").attr("masterPage") + ".htm", "utf8"); } catch (e) { response.masterPage = "NONE"; }
-		} catch (e) {
-			response.template = "NONE";
-		}
+		//-- load the template and master
+		fs.readFile(templateDir + "/" + path.templateName, "utf8",
+			function (err, data) {
+				console.log("TEMPLATE: " + path.templateName);
+				if (err) {
+					response.template = "NONE";
+					response.masterPage = "NONE";
+					merge(processRequest, processResponse, path, response);
+				} else {
+					response.template = data;
+					var tQ = cheerio.load(response.template);
+					fs.readFile(templateDir + "/" + tQ("head").attr("masterPage") + ".htm", "utf8",
+						function (err, data) {
+							if (err) { response.masterPage = "NONE"; } else { response.masterPage = data; }
+							merge(processRequest, processResponse, path, response);
+						}
+					);
+				}
+			}
+		);
 		//-- load template server side code behind script
-		try { response.templateConfig = fs.readFileSync(templateDir + "/" + path.templateConfigName, "utf8"); } catch (e) { response.templateConfig = "NONE"; }
+		fs.readFile(templateDir + "/" + path.templateConfigName, "utf8",
+			function (err, data) {
+				console.log("TEMPLATE: " + path.templateConfigName);
+				if (err) { response.templateConfig = "NONE"; } else { response.templateConfig = data; }
+				merge(processRequest, processResponse, path, response);
+			}
+		);
 		//-- load the content json file
-		try { response.content = fs.readFileSync(templateDir + "/content/" + path.contentFileName, "utf8"); } catch (e) { response.content = "NONE"; }
-		merge();
+		fs.readFile(templateDir + "/content/" + path.contentFileName, "utf8",
+			function (err, data) {
+				console.log("TEMPLATE: " + path.contentFileName);
+				if (err) { response.content = "NONE"; } else { response.content = data; }
+				merge(processRequest, processResponse, path, response);
+			}
+		);
 	}
 };
 
-var merge = function () {
+var merge = function (processRequest, processResponse, path, response) {
 	var allLoaded = true;
 	//-- reasons to wait
 	if (response.template == "LOADING") {allLoaded = false; }
@@ -130,18 +158,26 @@ var merge = function () {
 		//-- load template into cheerio, jquery style
 		var $ = cheerio.load(response.html);
 		//-- merge in content
-		if (response.content != "NONE" && response.content != "LOADING") {mergeContent($); }
+		if (response.content != "NONE" && response.content != "LOADING") {
+			mergeContent($, processRequest, processResponse, path, response);
+		}
+		//-- mearg in data
+		mergeData($, processRequest, processResponse, path, response);
 
-		var stringHTM = $.html();
 		//-- clear content edit
+		var stringHTM = $.html();
 		stringHTM = stringHTM.replace(/contenteditable="true"/g, " ");
 		//-- all done
-		res.writeHead(response.statusCode, { 'Content-Type': response.contentType });
-		res.end(stringHTM);
+		processResponse.writeHead(response.statusCode, { 'Content-Type': response.contentType });
+		processResponse.end(stringHTM);
 	}
 };
 
-var mergeContent = function ($) {
+var mergeData = function ($, processRequest, processResponse, path, response) {
+
+};
+
+var mergeContent = function ($, processRequest, processResponse, path, response) {
 	var content = null;
 	try { content = JSON.parse(response.content); } catch (e) { console.log(e); }
 	if (content != null) {
@@ -151,14 +187,17 @@ var mergeContent = function ($) {
 				$("#" + id).html(content[id]["text"]);
 			}
 		}
-	}
-	//-- apply metadata
-	if (content["metadata"] != null) {
-		$("head title").html(content["metadata"]["pageTitle"]["text"]);
+		//-- apply metadata
+		if (content["metadata"] != null) {
+			$("head title").html(content["metadata"]["pageTitle"]["text"]);
+		}
+	} else {
+		console.log("No Content for " + path.fileName);
 	}
 };
 
-var parseURL = function (uriString) {
+var parseURL = function (processRequest, path, response) {
+	var uriString = processRequest.url;
 	//-- split at ?
 	if (uriString.indexOf("?") > -1) {
 		var tmp = uriString.split("?");
