8085b36241e540ab1cf31ae0c0e0dc50e9995529
Mark Hebert <mark@hasissues.com>
2014-04-07 11:21:34 -0400

    update to VS 2013

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var http = null;
-var azure = null;
+http = null;
+azure = null;
 var fs = require("fs");
 var os = require("os");
 var local = require("./local.js");
 var settings = require("./config.js");
-var content = require("dhtmlplus");
-//var content = require("../dhtmlplus/index.js");
+var content = require("dhtmlplus"); //-- for deployment
+//var content = require("../dhtmlplus/index.js"); //-- for local development
+var serverOptions = { };
 
 //-- vars
 var configName = "LOCAL";
@@ -14,6 +15,8 @@ var machineName = os.hostname().toUpperCase();
 var port = 80;
 var useCloudData = false;
 var useCluster = false;
+var useHttpSys = false;
+var clusterForks = 2;
 
 //-- are we in Azure or IIS
 if (process.env.PORT != undefined) {
@@ -28,15 +31,20 @@ if (process.env.PORT != undefined) {
 	port = local.localKeys["port"];
 	useCloudData = local.localKeys["useCloudData"];
 	useCluster = local.localKeys["useCluster"];
-	if (useCluster) {
-		http = require("http");
-	} else {
+	useHttpSys = local.localKeys["useHttpSys"];
+	clusterForks = local.localKeys["clusterForks"] != null ? local.localKeys["clusterForks"] : numCPUs;
+	if (useHttpSys) {
+		//-- uses native http.sys on windows only
+		console.log("Using Native Mode HTTP.sys");
+		serverOptions = { "HTTPSYS_CACHE_DURATION": 0, "HTTPSYS_BUFFER_SIZE": 16384, "HTTPSYS_REQUEST_QUEUE_LENGTH": 10000, "HTTPSYS_PENDING_READ_COUNT": 5 };
 		http = require("httpsys").http();
+	} else {
+		http = require("http");
 	}
 }
 
 //-- HTTP Server for redirect
-var server = http.createServer(function (req, res) {
+var serverRequest = function (req, res) {
 	var requestHost = req.headers.host; 
 	var requestReferer = req.headers.referer;
 	var requestHttpVersion = req.httpVersion;
@@ -84,12 +92,22 @@ var server = http.createServer(function (req, res) {
 				var authConfig = auth({ authRealm: domain, authFile: "./" + settings.config[configName].endpoint[domain].auth + "-htpasswd", authType: settings.config[configName].endpoint[domain].auth });
 				authConfig.apply(req, res, function (username) {
 					//-- process request with security
-					content.presenter(req, res, domain, settings.config[configName], useCloudData, configName, username, local);
+					try {
+						content.presenter(req, res, domain, settings.config[configName], useCloudData, configName, username, local);
+					} catch (e) {
+						res.writeHead(500, { 'Content-Type': 'text/html'});
+						res.end(e);
+					}
 				});
 			}
 		} else {
 			//-- process request without security
-			content.presenter(req, res, domain, settings.config[configName], useCloudData, configName, "", local);
+			try {
+				content.presenter(req, res, domain, settings.config[configName], useCloudData, configName, "", local);
+			} catch (e) {
+				res.writeHead(500, { 'Content-Type': 'text/html'});
+				res.end(e);
+			}
 		}
 	} else {
 		//-- not processed
@@ -100,27 +118,24 @@ var server = http.createServer(function (req, res) {
 			console.log("NOT HOSTED: " + domain + " REFERER: " + requestReferer);
 		}
 	}
-});
+};
+
+//-- start listener based on config
 if (configName == "LOCAL" && useCluster) {
 	var cluster = require("cluster");
 	if (cluster.isMaster) {
 		console.log("Machine " + machineName  + " with " + numCPUs + " CPUs.");
 		console.log("Running Node Version " + process.versions.node);
-		// Fork workers. one less then max cpu count
-		for (var i = 1; i <= numCPUs; i++) { cluster.fork(); }
-		cluster.on("exit", function(worker, code, signal) {
-			var exitCode = worker.process.exitCode;
-			console.log("worker " + worker.process.pid + " died (" + exitCode + "). restarting...");
-			cluster.fork();
-		});
-		cluster.on('listening', function(worker, address) {
-			console.log("Worker " + worker.process.pid + " listening  on " + address.port);
-		});
+		for (var i = 1; i <= clusterForks; i++) { cluster.fork(); }
+		cluster.on("exit", function(worker, code, signal) { var exitCode = worker.process.exitCode; console.log("worker " + worker.process.pid + " died (" + exitCode + "). restarting..."); cluster.fork(); });
+		cluster.on('listening', function(worker, address) { console.log("Worker " + worker.process.pid + " listening  on " + address.port); });
 	} else {
 		// Workers can share any TCP connection, In this case its a HTTP server
-		server.listen(port);
+		var server = http.createServer(function (req, res) { serverRequest(req, res); }).listen(port);
 	}
-} else if (configName == "LOCAL" && !useCluster) {
+} else if (configName == "LOCAL" && !useCluster && useHttpSys) {
+	console.log("Machine " + machineName  + " with " + numCPUs + " CPUs.");
+	console.log("Running Node Version " + process.versions.node);
 	for (var domain in settings.config.LOCAL.endpoint) {
 		var webServerAddress = "http://";
 		if (settings.config.LOCAL.endpoint[domain].subDomain == "") {
@@ -128,8 +143,12 @@ if (configName == "LOCAL" && useCluster) {
 		} else {
 			webServerAddress += settings.config.LOCAL.endpoint[domain].subDomain + "." + domain + ":" + settings.config.LOCAL.endpoint[domain].port + "/";
 		}
-		console.log("WEB SERVER: " + webServerAddress);
-		server.listen(webServerAddress);
+		try {
+			var server = http.createServer(serverOptions, function (req, res) { serverRequest(req, res); }).listen(webServerAddress);
+			console.log("WEB SERVER: " + webServerAddress);
+		} catch (e) {
+			console.log("FAIL WEB SERVER: " + webServerAddress);
+		}
 	}
 	for (var domain in settings.config.LOCAL.redirect) {
 		var webServerAddress = "http://";
@@ -138,12 +157,16 @@ if (configName == "LOCAL" && useCluster) {
 		} else {
 			webServerAddress += settings.config.LOCAL.redirect[domain].subDomain + "." + domain + ":" + settings.config.LOCAL.redirect[domain].subDomainPort + "/";
 		}
-		console.log("REDIRECT: " + webServerAddress);
-		server.listen(webServerAddress);
+		try {
+			var server = http.createServer(serverOptions, function (req, res) { serverRequest(req, res); }).listen(webServerAddress);
+			console.log("REDIRECT: " + webServerAddress);
+		} catch (e) {
+			console.log("FAIL REDIRECT: " + webServerAddress);
+		}
 	}
 } else {
-	server.listen(port);
+	var server = http.createServer(function (req, res) { serverRequest(req, res); }).listen(port);
 	console.log("Machine " + machineName  + " with " + numCPUs + " CPUs.");
 	console.log("Running Node Version " + process.versions.node);
-	console.log("Listing  on port " + port);
+	console.log("Listing on port " + port);
 }
